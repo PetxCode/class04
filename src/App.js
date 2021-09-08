b472359beecc4a529a8248a29b90b1e37d31ab98
PetxCode <71887594+PetxCode@users.noreply.github.com>
2021-09-08 20:50:34 +0100

    a test commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WorkHome from "./component/WorkHome";
+
+const App = () => {
+  return (
+    <div>
+      <WorkHome />
+    </div>
+  );
+};
+export default App;
